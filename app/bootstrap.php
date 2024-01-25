8594296a736a3e721b1295fd3d15396f42873e0a
court-tek <dishoughng@gmail.com>
2024-01-24 20:32:54 -0500

    initial second commit

---
+++ app/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+    // load libraries
+    require_once '../app/libraries/Core.php';
+    require_once '../app/libraries/Controller.php';
+    require_once '../app/libraries/Database.php'; 
---
+++ app/libraries/Core.php
@@ -0,0 +1,15 @@
+<?php
+
+    class Core {
+        protected $currentController = 'Pages';
+        protected $currentMethod = 'index';
+        protected $params = []; 
+
+        public function __construct() {
+            $this->getUrl();
+        }
+
+        public function getUrl() {
+            echo $_GET['url'];
+        }
+    }
