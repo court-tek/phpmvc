a7b74e9834b9b854d3640a71c1484d0f50a78e9f
court-tek <dishoughng@gmail.com>
2024-01-25 17:32:59 -0500

    added url and sitename to config

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,8 @@
 <?php
-    // load libraries
-    require_once '../app/libraries/Core.php';
-    require_once '../app/libraries/Controller.php';
-    require_once '../app/libraries/Database.php'; 
+    // load config
+    require_once '../app/config/config.php';
+
+    // autoload core libraries
+    spl_autoload_register(function($className) {
+        require_once '../app/libraries/' . $className . '.php';
+    });
