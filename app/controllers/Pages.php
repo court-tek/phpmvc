5177e1f85121c77078d2a2b168e288b67a5edb9d
court-tek <dishoughng@gmail.com>
2024-01-25 10:06:07 -0500

    set up controller class

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -2,12 +2,27 @@
     class Pages {
         public function __construct()
         {
-            echo 'Pages controller loaded';
+            // echo 'Pages controller loaded';
         }
 
         public function index()
         {
-            
+            echo 'This is the index page';
+        }
+
+        public function new()
+        {
+            echo 'This is the new page';
+        }
+
+        public function show()
+        {
+            echo 'This is the show page';
+        }
+
+        public function edit()
+        {
+            echo 'This is the edit page';
         }
 
         public function about($id) 
