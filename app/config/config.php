49bfab1cb54845029f1271baf94368b9bd5bb19a
court-tek <dishoughng@gmail.com>
2024-01-25 11:37:59 -0500

    added url and sitename to config

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+    // App root
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    // URL root
+    define('URLROOT', 'http://localhost/phpmvc');
+
+    // Site name
+    define('SITENAME', 'PHP MVC');
